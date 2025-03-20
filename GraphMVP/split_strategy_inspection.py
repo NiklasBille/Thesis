7cc44f9697c4ae9b207d132a9aaccd9a0e4aea1d
rasmusthrane <re.thrane@hotmail.dk>
2025-03-20 12:23:18 +0000

    we can now also check the splits for lipo

--- GraphMVP/split_strategy_inspection.py
+++ GraphMVP/split_strategy_inspection.py
@@ -9,9 +9,10 @@ from datasets_complete_feature import MoleculeDatasetComplete
 from torch_geometric.data import DataLoader
 
 if __name__ == "__main__":
-    dataset_folder = "/workspace/datasets/molecule_datasets/freesolv"
+    task_name = "lipophilicity"
+    dataset_folder = f"/workspace/datasets/molecule_datasets/{task_name}"
 
-    dataset = MoleculeDatasetComplete(dataset_folder, dataset="freesolv")
+    dataset = MoleculeDatasetComplete(dataset_folder, dataset=f"{task_name}")
     smiles_list = pd.read_csv(dataset_folder + '/processed/smiles.csv', header=None)[0].tolist()
 
     train_dataset, valid_dataset, test_dataset = scaffold_split(
@@ -25,5 +26,4 @@ if __name__ == "__main__":
         test_indices.append(data_id)
         test_SMILES.append(smiles_list[data_id])
 
-
-    print(f"\n===SMILES of molecules in test set===\n{test_SMILES}")
+    print(f"\n===SMILES of molecules in test set for {task_name}===\n{test_SMILES}")
