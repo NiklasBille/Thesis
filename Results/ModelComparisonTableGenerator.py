2c785e816959f9f88160fa9c54d2670a69020a8c
rasmusthrane <re.thrane@hotmail.dk>
2025-05-19 11:29:31 +0200

    added raw result comparison for split experiments

--- Results/ModelComparisonTableGenerator.py
+++ Results/ModelComparisonTableGenerator.py
@@ -29,6 +29,13 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
                 [possible_sub_experiments, self.allowed_models],
                 names=["sub_experiment", "model"]
                 )
+        else:
+            possible_sub_experiments = ["random", "scaff"]
+            train_props = ["train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+            columns = pd.MultiIndex.from_product(
+                [possible_sub_experiments, train_props, self.allowed_models],
+                names=["sub_experiment", "train_prop", "model"]
+            )
             
         table = pd.DataFrame(index=self.datasets, columns=columns)
 
@@ -39,10 +46,19 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
         for model, raw_tables in raw_table_dict.items():
             raw_table = raw_tables[metric_type]
             
-            # Add each sub experiment metrics to the correct colunm
-            for sub_experiment in possible_sub_experiments:
-                R = raw_table.loc[self.datasets, sub_experiment]
-                table.loc[:, (sub_experiment, model)] = R['mean']
+            # The table for each experiment is different, so they are processed in different ways
+            if self.experiment == 'noise':
+                # Add each sub experiment metrics to the correct colunm
+                for sub_experiment in possible_sub_experiments:
+                    R = raw_table.loc[self.datasets, sub_experiment]
+                    table.loc[:, (sub_experiment, model)] = R['mean']
+            
+            elif self.experiment =='split':
+
+                for sub_experiment in possible_sub_experiments:
+                    for train_prop in train_props:
+                        R = raw_table.loc[self.datasets, (sub_experiment, train_prop)]
+                        table.loc[:, (sub_experiment, train_prop, model)] = R['mean']
 
         return table
 
@@ -65,9 +81,27 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
             self.round_table(primary_table)
             self.round_table(secondary_table)
         print("\n PRIMARY METRIC")
-        print(primary_table.to_string())
-        print("\n" + "-"*80)
-        if print_secondary_metric:
-            print("\n SECONDARY METRIC")
-            print(secondary_table.to_string())
+    
+        if self.experiment == 'noise':
+            print(primary_table.to_string())
             print("\n" + "-"*80)
+            if print_secondary_metric:
+                print("\n SECONDARY METRIC")
+                print(secondary_table.to_string())
+                print("\n" + "-"*80)
+            
+        # Split table is very large so we present it as two tables, one for random and one for scaffold
+        else: 
+            print("[RANDOM]")
+            primary_table.set_index('metric', append=True, inplace=True) # keep metric column when slicing
+            print(primary_table.loc[:, 'random'].to_string(), '\n')
+            print("[SCAFFOLD]")
+            print(primary_table.loc[:, 'scaff'].to_string(), '\n', '-'*80)
+
+            if print_secondary_metric:
+                secondary_table.set_index('metric', append=True, inplace=True) # keep metric column when slicing
+                print("\n SECONDARY METRIC")
+                print("[RANDOM]")
+                print(secondary_table.loc[:, 'random'].to_string(), '\n')
+                print("[SCAFFOLD]")
+                print(secondary_table.loc[:, 'scaff'].to_string(), '\n', '-'*80)
