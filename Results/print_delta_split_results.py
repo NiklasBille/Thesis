e8c0207ad153b6cbcf9822501cf995d81b48fe75
rasmusthrane <re.thrane@hotmail.dk>
2025-05-19 18:39:51 +0200

    defaults to --partition test now

--- Results/print_delta_split_results.py
+++ Results/print_delta_split_results.py
@@ -4,7 +4,7 @@ import DeltaSplitTableGenerator as dstg
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate metric tables for experiments.")
     parser.add_argument('--model', required=True, choices=["3DInfomax", "GraphMVP", "GraphCL"], help="Model name")
-    parser.add_argument('--partition', required=True, choices=["train", "val", "test"], help="Data partition")
+    parser.add_argument('--partition', default='test', choices=["train", "val", "test"], help="Data partition")
     parser.add_argument('--print_decimals', default=3, type=int, help="How many decimals to print")
     parser.add_argument('--print_secondary_metric', action='store_true', help="Whether the table with secondary metrics is part of output")
     args = parser.parse_args()
--- Results/print_raw_results.py
+++ Results/print_raw_results.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate metric tables for experiments.")
     parser.add_argument('--model', required=True, choices=["3DInfomax", "GraphMVP", "GraphCL", "all"], help="Model name. If set to all, all models are compared")
     parser.add_argument('--experiment', required=True, choices=["noise", "split"], help="Experiment type")
-    parser.add_argument('--partition', required=True, choices=["train", "val", "test"], help="Data partition")
+    parser.add_argument('--partition', default='test', choices=["train", "val", "test"], help="Data partition")
     parser.add_argument('--print_decimals', default=3, type=int, help="How many decimals to print")
     parser.add_argument('--print_secondary_metric', action='store_true', help="Whether the table with secondary metrics is part of output")
     args = parser.parse_args()
