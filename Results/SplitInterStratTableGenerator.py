4ed93944b08e8551ae518d0633e8fc1f3611cb67
rasmusthrane <re.thrane@hotmail.dk>
2025-05-31 14:21:46 +0200

    WIP estimating the variance of delta

--- Results/SplitInterStratTableGenerator.py
+++ Results/SplitInterStratTableGenerator.py
@@ -1,6 +1,7 @@
 import sys
 from typing_extensions import override #to explicitly state when overriding method
 import pandas as pd
+import numpy as np
 
 import RawTableGenerator as tg
 
@@ -8,28 +9,60 @@ import RawTableGenerator as tg
 class SplitInterStratTableGenerator(tg.RawTableGenerator):
     def __init__(self, model, experiment, partition, decimals=None):
         super().__init__(model, experiment, partition, decimals)
+        self.model=model
         self.raw_primary_table, self.raw_secondary_table = super().create_table(experiment, model, partition)
 
-    def _compute_delta_table(self, df):
+    def _compute_delta_table(self, df, use_secondary_metric=False):
         
         # Create empty table to hold delta values
-        columns = ["metric", "train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+        #columns = ["metric", "train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+        #delta_table = pd.DataFrame(index=self.datasets, columns=columns)
+
+        train_props = ["train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+        columns = pd.MultiIndex.from_product(
+            [train_props, ["mean", "std"]],
+            names=["train_prop", "stat"]
+        )
+    
         delta_table = pd.DataFrame(index=self.datasets, columns=columns)
 
-        for index, row in df.iterrows():
-            R_random = row.loc['random']
-            R_scaffold = row.loc['scaff']
-
-            # Remove std from the table
-            R_scaffold = R_scaffold.drop(index=R_scaffold.index[R_scaffold.index.get_level_values('stat') == 'std'])
-            R_random = R_random.drop(index=R_random.index[R_random.index.get_level_values('stat') == 'std'])
-
-            # Remove the 'stat' level, but keep the corresponding values
-            R_scaffold = R_scaffold.droplevel('stat')
-            R_random = R_random.droplevel('stat')
+        for dataset, row in df.iterrows():
+            # First compute the delta value
+            R_random = row.loc['random']; R_scaffold = row.loc['scaff']
+            R_random_mean = R_random[:, "mean"]; R_scaffold_mean = R_scaffold[:, "mean"]
+            R_random_std = R_random[:, "std"]; R_scaffold_std = R_scaffold[:, "std"]
             
-            delta = (R_scaffold - R_random)
-            delta_table.loc[index] = delta
+            delta = (R_scaffold_mean - R_random_mean)
+
+            # Then approximate the standard deviation of delta for each train_prop:
+            std_vector = []
+            for tp in train_props:
+                results_random = self.extract_results_split(model=self.model, dataset=dataset, split_strategy='random', train_prop=tp)
+                results_scaffold = self.extract_results_split(model=self.model, dataset=dataset, split_strategy='scaff', train_prop=tp)
+
+                if dataset == 'bbbp' and tp == 'train_prop=0.6':
+                    continue
+
+                if dataset in ['freesolv', 'esol', 'lipo']:
+                    metric = 'mae' if use_secondary_metric else 'rmse'                                
+                else:
+                    metric = 'prcauc' if use_secondary_metric else 'rocauc'
+                X = results_scaffold[f'{self.partition}_{metric}']
+                Y = results_random[f'{self.partition}_{metric}']
+
+                n = len(X)
+                sigma2_X = np.var(X,ddof=1)
+                sigma2_Y = np.var(Y, ddof=1)
+                cov = np.cov(X,Y)[0, 1]
+                approx_std = np.sqrt(sigma2_X/n + sigma2_Y/n - 2*cov/n)
+                std_vector.append(approx_std) 
+
+            for i, tp in enumerate(train_props):
+                if dataset == 'bbbp' and tp == 'train_prop=0.6':
+                    continue
+                delta_table.loc[dataset, (tp, 'mean')] = delta[i]
+                delta_table.loc[dataset, (tp, 'std')] = std_vector[i]
+
 
         delta_table['metric'] = df['metric']     
 
@@ -39,7 +72,7 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
     @override
     def create_table(self):
         primary_table = self._compute_delta_table(self.raw_primary_table)
-        secondary_table = self._compute_delta_table(self.raw_secondary_table)
+        secondary_table = self._compute_delta_table(self.raw_secondary_table, use_secondary_metric=True)
 
         return primary_table, secondary_table
 
@@ -60,5 +93,9 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
             print("SECONDARY METRIC DELTA TABLE")
             print(secondary_table.to_string())
             print("\n" + "-"*80)
+
+    @override
+    def convert_table_to_latex(self, print_secondary_metric=False):
+        pass
 
         
--- Results/print_split_interstrat_results.py
+++ Results/print_split_interstrat_results.py
@@ -3,14 +3,14 @@ import SplitInterStratTableGenerator as InterStratTG
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate metric tables for experiments.")
-    parser.add_argument('--model', required=True, choices=["3DInfomax", "GraphMVP", "GraphCL_1", "GraphCL_2", "all"], help="Model name. If set to all, all models are compared")
+    parser.add_argument('--model', required=True, choices=["3DInfomax", "GraphMVP", "GraphCL_1", "GraphCL_2"], help="Model name.")
     parser.add_argument('--partition', default='test', choices=["train", "val", "test"], help="Data partition")
     parser.add_argument('--print_decimals', default=3, type=int, help="How many decimals to print")
     parser.add_argument('--print_secondary_metric', action='store_true', help="Whether the table with secondary metrics is part of output")
+    parser.add_argument('--to_latex', action='store_true', help="Whether to print the table as LaTeX")
     args = parser.parse_args()
 
-
-    # Create an instance of the DeltaTableGenerator class
+    # Create an instance of the SplitInterStratTableGenerator class
     inter_strat_table_generator = InterStratTG.SplitInterStratTableGenerator(
         model=args.model,
         experiment='split',
@@ -19,4 +19,7 @@ if __name__ == "__main__":
     )
 
     inter_strat_table_generator.print_result_table(print_secondary_metric=args.print_secondary_metric)
+
+    if args.to_latex is True:
+        inter_strat_table_generator.convert_table_to_latex(print_secondary_metric=args.print_secondary_metric)
 
