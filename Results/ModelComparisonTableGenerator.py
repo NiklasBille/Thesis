0ff7093e971631b98e5fbe77af0fd9c76995c06e
rasmusthrane <re.thrane@hotmail.dk>
2025-06-01 10:09:49 +0200

    can now output inter-strat table to LaTeX

--- Results/ModelComparisonTableGenerator.py
+++ Results/ModelComparisonTableGenerator.py
@@ -7,7 +7,7 @@ from typing import Literal
 from typing_extensions import override #to explicitly state when overriding method
 
 def approx_variance_delta_method(X, Y):
-    n = len(X)
+    n = len(X)   
 
     mu_X = np.mean(X)
     mu_Y = np.mean(Y)
@@ -135,12 +135,14 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
         readout_results = self.extract_results_noise(model, dataset, noise_level)
         
         if dataset in ['freesolv', 'esol', 'lipo']:
-            metric = 'mae' if use_secondary_metric else 'rmse'                                
+            metric = 'mae' if use_secondary_metric else 'rmse' 
+            X = readout_results[f'{partition}_{metric}']
+            Y = baseline_results[f'{partition}_{metric}']                               
         else:
             metric = 'prcauc' if use_secondary_metric else 'rocauc'
+            X = [100*result for result in readout_results[f'{partition}_{metric}']]
+            Y = [100*result for result in baseline_results[f'{partition}_{metric}']]
 
-        X = readout_results[f'{partition}_{metric}']
-        Y = baseline_results[f'{partition}_{metric}']
                                                                 
         std_noise = np.sqrt(approx_variance_delta_method(X, Y))
 
@@ -185,11 +187,10 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
         # Dictionaries for rename mapping
         metric_rename = {"rmse": "RMSE $\\downarrow$", "mae": "MAE $\\downarrow$", "rocauc": "ROC-AUC $\\uparrow$", "prcauc": "PRC-AUC $\\uparrow$"}
         dataset_rename = {"freesolv": "FreeSolv $\\downarrow$", "esol": "ESOL $\\downarrow$", "lipo": "Lipo $\\downarrow$", "bace": "BACE $\\uparrow$", "bbbp": "BBBP $\\uparrow$", "clintox": "ClinTox $\\uparrow$", "hiv": "HIV $\\uparrow$", "muv": "MUV $\\uparrow$", "sider": "SIDER $\\uparrow$", "toxcast": "ToxCast $\\uparrow$", "tox21": "Tox21 $\\uparrow$"}
-
         # For noise experiment
         if self.experiment == "noise":
             possible_sub_experiments = ["noise=0.0", "noise=0.05", "noise=0.1", "noise=0.2"] 
-
+            
             # Populate the table
             for sub_exp in possible_sub_experiments:
                 for model in self.list_of_models:
@@ -312,3 +313,94 @@ class ModelComparisonTableGenerator(tg.RawTableGenerator):
             latex_str_scaff = "\\resizebox{\\textwidth}{!}{%\n" + latex_str_scaff + "}"
             print(latex_str_random, "\n")
             print(latex_str_scaff)
+
+
+class ModelComparisonSplitInterStratTableGenerator(ModelComparisonTableGenerator):
+    def __init__(self, experiment, partition, list_of_models, decimals=None):
+        super().__init__(experiment=experiment, partition=partition, list_of_models=list_of_models, decimals=decimals)
+
+
+    @override
+    def print_result_table(self, print_secondary_metric=False):
+        print("\n" + "="*80)
+        print(f"EXPERIMENT: {self.experiment} | PARTITION: {self.partition} | Comparing models")
+        print("="*80)
+        primary_table, secondary_table = self.create_table()
+        if self.decimals is not None:
+            self.round_table(primary_table)
+            self.round_table(secondary_table)
+
+        if print_secondary_metric is False:
+            print("PRIMARY METRIC")
+            print(primary_table.to_string())
+            print("\n" + "-"*80)
+        else:
+            print("SECONDARY METRIC")
+            print(secondary_table.to_string())
+            print("\n" + "-"*80)
+
+    @override
+    def _compute_model_comparison_table(self, raw_table_dict, metric_type: Literal['primary', 'secondary']):
+
+        # Create empty MultiIndex table
+        train_props = ["train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+        columns = pd.MultiIndex.from_product(
+            [train_props, self.list_of_models, ["mean", "std"]],
+            names=["train_prop", "model", "stat"]
+        )
+        
+        table = pd.DataFrame(index=self.datasets, columns=columns)
+
+        # Insert a column for information on metrics
+        first_entry_key = list(raw_table_dict)[0]
+        table.insert(0, 'metric', raw_table_dict[first_entry_key][metric_type]['metric'])
+
+        # Fill table
+        for model, raw_tables in raw_table_dict.items():
+            raw_table = raw_tables[metric_type]
+            for train_prop in train_props:
+                R = raw_table.loc[self.datasets, (train_prop)]
+                table.loc[:, (train_prop, model, "mean")] = R["mean"]
+                table.loc[:, (train_prop, model, "std")] = R["std"]
+
+        return table
+    
+    @override
+    def convert_table_to_latex(self, print_secondary_metric=False):
+        table_primary_metric, table_secondary_metric = self.create_table()
+        table = table_primary_metric if print_secondary_metric is False else table_secondary_metric
+
+        mean_columns = table.columns.get_level_values('stat') == 'mean'
+        columns_without_stat = table.columns[mean_columns].droplevel('stat')
+
+        combined = pd.DataFrame(index=table.index, columns=columns_without_stat)
+
+        train_props = ["train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+        # First populate the table
+        for train_prop in train_props:
+            for model in self.list_of_models:
+                mean_col = (train_prop, model, "mean")
+                std_col = (train_prop, model, "std")
+    
+                combined[train_prop, model] = table.apply(
+                    lambda row: f"{row[mean_col]:.{self.decimals}f}$\\pm${row[std_col]:.{self.decimals}f}" 
+                                if pd.notna(row[mean_col]) and pd.notna(row[std_col]) 
+                                else "*",
+                    axis=1
+                )
+
+        rename_mapping = {f"train_prop={p}": f"$p={p}$" for p in [0.8, 0.7, 0.6]}
+        combined.rename(columns=rename_mapping, inplace=True)
+
+        dataset_rename = {"freesolv": "FreeSolv $\\downarrow$", "esol": "ESOL $\\downarrow$", "lipo": "Lipo $\\downarrow$", "bace": "BACE $\\uparrow$", "bbbp": "BBBP $\\uparrow$", "clintox": "ClinTox $\\uparrow$", "hiv": "HIV $\\uparrow$", "muv": "MUV $\\uparrow$", "sider": "SIDER $\\uparrow$", "toxcast": "ToxCast $\\uparrow$", "tox21": "Tox21 $\\uparrow$"}
+        combined.rename(index=dataset_rename, inplace=True)
+            
+        combined.columns.names = [None, None] # Get rid of 'train_prop' and 'model' naming
+
+        latex_str = combined.to_latex()
+
+        latex_str = latex_str.replace(rf'\multicolumn{{{len(self.list_of_models)}}}{{r}}', rf'\multicolumn{{{len(self.list_of_models)}}}{{c}}')
+
+        # Wrap entire str in a resize box
+        latex_str = "\\resizebox{\\textwidth}{!}{%\n" + latex_str + "}"
+        print(latex_str)
--- Results/SplitInterStratTableGenerator.py
+++ Results/SplitInterStratTableGenerator.py
@@ -13,7 +13,6 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
         self.raw_primary_table, self.raw_secondary_table = super().create_table(experiment, model, partition)
 
     def _compute_delta_table(self, df, use_secondary_metric=False):
-        
         # Create empty table to hold delta values
         #columns = ["metric", "train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
         #delta_table = pd.DataFrame(index=self.datasets, columns=columns)
@@ -34,9 +33,8 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
             
             delta = (R_scaffold_mean - R_random_mean)
 
-            # Then approximate the standard deviation of delta for each train_prop:
-            std_vector = []
-            for tp in train_props:
+            # Then approximate the standard deviation of delta for each train_prop
+            for i, tp in enumerate(train_props):
                 results_random = self.extract_results_split(model=self.model, dataset=dataset, split_strategy='random', train_prop=tp)
                 results_scaffold = self.extract_results_split(model=self.model, dataset=dataset, split_strategy='scaff', train_prop=tp)
 
@@ -44,27 +42,24 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
                     continue
 
                 if dataset in ['freesolv', 'esol', 'lipo']:
-                    metric = 'mae' if use_secondary_metric else 'rmse'                                
+                    metric = 'mae' if use_secondary_metric else 'rmse'       
+                    X = results_scaffold[f'{self.partition}_{metric}']
+                    Y = results_random[f'{self.partition}_{metric}']                         
                 else:
                     metric = 'prcauc' if use_secondary_metric else 'rocauc'
-                X = results_scaffold[f'{self.partition}_{metric}']
-                Y = results_random[f'{self.partition}_{metric}']
+                    X = [100*result for result in results_scaffold[f'{self.partition}_{metric}']]
+                    Y = [100*result for result in results_random[f'{self.partition}_{metric}']]
 
                 n = len(X)
-                sigma2_X = np.var(X,ddof=1)
-                sigma2_Y = np.var(Y, ddof=1)
-                cov = np.cov(X,Y)[0, 1]
+                sigma2_X = np.var(X, ddof=1)
+                sigma2_Y = np.var(Y, ddof=1)                
+                cov = np.cov(X,Y, ddof=1)[0, 1]
                 approx_std = np.sqrt(sigma2_X/n + sigma2_Y/n - 2*cov/n)
-                std_vector.append(approx_std) 
-
-            for i, tp in enumerate(train_props):
-                if dataset == 'bbbp' and tp == 'train_prop=0.6':
-                    continue
+                
                 delta_table.loc[dataset, (tp, 'mean')] = delta[i]
-                delta_table.loc[dataset, (tp, 'std')] = std_vector[i]
+                delta_table.loc[dataset, (tp, 'std')] = approx_std
 
-
-        delta_table['metric'] = df['metric']     
+        delta_table.insert(loc=0, column='metric', value=df['metric'])
 
         return delta_table
 
@@ -96,6 +91,37 @@ class SplitInterStratTableGenerator(tg.RawTableGenerator):
 
     @override
     def convert_table_to_latex(self, print_secondary_metric=False):
-        pass
+        table_primary_metric, table_secondary_metric = self.create_table()
+        table = table_primary_metric if print_secondary_metric is False else table_secondary_metric
+
+        # Create a table to hold "mean \pm std"
+        combined = pd.DataFrame(index=table.index)
+
+        # Dictionaries for rename mapping
+        metric_rename = {"rmse": "RMSE $\\downarrow$", "mae": "MAE $\\downarrow$", "rocauc": "ROC-AUC $\\uparrow$", "prcauc": "PRC-AUC $\\uparrow$"}
+        dataset_rename = {"freesolv": "FreeSolv", "esol": "ESOL", "lipo": "Lipo", "bace": "BACE", "bbbp": "BBBP", "clintox": "ClinTox", "hiv": "HIV", "muv": "MUV", "sider": "SIDER", "toxcast": "ToxCast", "tox21": "Tox21"}
+
+        train_props = ["train_prop=0.8", "train_prop=0.7", "train_prop=0.6"]
+
+        # First populate the table
+        for train_prop in train_props:
+        
+            mean_col = (train_prop, "mean")
+            std_col = (train_prop, "std")
+
+            combined[train_prop] = table.apply(
+                lambda row: f"{row[mean_col]:.{self.decimals}f}$\\pm${row[std_col]:.{self.decimals}f}" 
+                            if pd.notna(row[mean_col]) and pd.notna(row[std_col]) 
+                            else "*",
+                axis=1
+            )
+
+        # Then rename columns and rows accordingly
+        rename_mapping = {f"train_prop={p}": f"$p={p}$" for p in [0.8, 0.7, 0.6]}
+        combined.rename(columns=rename_mapping, inplace=True)
+        combined.rename(index=dataset_rename, inplace=True)
+
+        latex_str = combined.to_latex()
+        print(latex_str)
 
         
